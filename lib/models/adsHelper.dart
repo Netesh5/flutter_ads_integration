1e08117e2a278f2970c5bd025de0d23a86d919e0
Netesh5 <netesh.paudel@gmail.com>
2022-04-28 22:42:26 +0545

    Ads id

--- lib/models/adsHelper.dart
+++ lib/models/adsHelper.dart
@@ -8,4 +8,20 @@ class AdHelper {
       return "ca-app-pub-2951068600361614/5710532619";
     }
   }
+
+  String DetailsBanner() {
+    if (Platform.isIOS) {
+      return "ca-app-pub-2951068600361614/1675084681";
+    } else {
+      return "ca-app-pub-2951068600361614/4397450948";
+    }
+  }
+
+  String fullPageAds() {
+    if (Platform.isIOS) {
+      return "ca-app-pub-2951068600361614/5422758000";
+    } else {
+      return "ca-app-pub-2951068600361614/3084369273";
+    }
+  }
 }
